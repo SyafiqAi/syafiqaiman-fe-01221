78450ab438b1e1d23753bb2b502f5ada3587646a
syafiq <isyafiq@gmail.com>
2024-05-17 14:37:08 +0800

    working prototype

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./GlobalRedux/provider";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <StoreProvider>
           {children}
-        </Providers>
+        </StoreProvider>
       </body>
     </html>
   );
---
+++ app/page.jsx
@@ -0,0 +1,49 @@
+'use client';
+import Image from "next/image";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount, fetchMoviesByTheater, fetchMoviesByTimeSlot } from '../lib/features/searchMovies/searchMoviesSlice'
+
+export default function Home() {
+  const status = useSelector(state => state.searchMovies.status)
+  const movies = useSelector(state => state.searchMovies.movies)
+  const dispatch = useDispatch()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let query = e.target[0].value
+    dispatch(fetchMoviesByTimeSlot({timeStart: '1', timeEnd: '1'}))
+  }
+
+  let content;
+
+  if (status === 'loading') {
+    content = <p>loading</p>
+  } else if (status === 'succeeded') {
+    content = <p>{movies}</p>
+  } else if (status === 'failed') {
+    content = <p>failed</p>
+  }
+
+  return (
+    <div>
+      <div>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" name="" id="" />
+          <button
+            aria-label="Increment value"
+            type='submit'
+          >
+            Search
+          </button>
+        </form>
+
+        <h2>movies:</h2>
+        <div>
+          {content}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
