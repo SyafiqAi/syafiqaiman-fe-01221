2e9f451413cb2dc013a9a472b0a8b5588a7a7f5c
syafiq <isyafiq@gmail.com>
2024-05-19 00:23:12 +0800

    active link styling

---
+++ app/components/NavbarLink.jsx
@@ -0,0 +1,20 @@
+import { Link } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
+
+export default function NavbarLink(props) {
+    let href = props.href
+    const pathName = usePathname();
+
+    let isActive = (href) => { return href === pathName };
+
+    return (
+        <Link 
+            href={href} 
+            _hover={{ borderBottom:'4px', borderColor:'yellow' }} 
+            borderBottom={isActive(href) ? '4px' : ''}
+            borderColor={isActive(href) ? 'yellow' : ''}
+        >
+            {props.children}
+        </Link>
+    )
+}
--- app/components/TopNavbar.jsx
+++ app/components/TopNavbar.jsx
@@ -1,45 +1,49 @@
 'use client'
 import { Link } from '@chakra-ui/next-js'
-import { Avatar, Box, Flex, Popover, Spacer, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, HStack, Popover, Spacer, Text } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 import NavbarSearchButton from './NavbarSearchButton'
+import { usePathname } from 'next/navigation';
+import NavbarLink from './NavbarLink'
 
 export default function TopNavbar() {
 
     const navPadding = 14
-    
+
     return (
         <Flex p={7} position='absolute' width={'full'} px={20} align={'center'} overflowY={'visible'} overflowX={'clip'} >
-            <h1 className='inline'>
-                <Text fontSize='3xl' fontWeight={'bold'}>
-                    PcariMovie
-                </Text>
-            </h1>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                Home
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                Movies
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                TV Shows
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                Videos
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                FAQ
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                Pricing
-            </Link>
-            <Link href='/' _hover={{ color: 'yellow' }} ml={navPadding}>
-                Contact Us
-            </Link>
+            <HStack spacing={14}>
+                <h1 className='inline'>
+                    <Text fontSize='3xl' fontWeight={'bold'}>
+                        PcariMovie
+                    </Text>
+                </h1>
+                <NavbarLink href='/'>
+                    Home
+                </NavbarLink>
+                <NavbarLink href='/movies'>
+                    Movies
+                </NavbarLink>
+                <NavbarLink href='/tv_shows'>
+                    TV Shows
+                </NavbarLink>
+                <NavbarLink href='/videos'>
+                    Videos
+                </NavbarLink>
+                <NavbarLink href='/faq'>
+                    FAQ
+                </NavbarLink>
+                <NavbarLink href='/pricing'>
+                    Pricing
+                </NavbarLink>
+                <NavbarLink href='/contact_us'>
+                    Contact Us
+                </NavbarLink>
+            </HStack>
             <Spacer />
-            
+
             <NavbarSearchButton />
-            <Avatar size='sm' name='John Glich'  ml={navPadding} />
+            <Avatar size='sm' name='John Glich' ml={navPadding} />
             <Text ml={4}>John Glich</Text>
 
         </Flex>
