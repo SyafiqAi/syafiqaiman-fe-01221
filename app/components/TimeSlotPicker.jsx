0ed4e564effc5b3842e35c2be73911b7c53eade3
syafiq <isyafiq@gmail.com>
2024-05-18 23:42:42 +0800

    added date picker

---
+++ app/components/TimeSlotPicker.jsx
@@ -0,0 +1,32 @@
+import { useState, forwardRef } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+import { CalendarIcon, TimeIcon } from "@chakra-ui/icons";
+
+export default function Picker() {
+    const [startDate, setStartDate] = useState(null);
+    const ChakraCustomInput = forwardRef(({ value, onClick }, ref) => (
+
+        <InputGroup onClick={onClick} ref={ref}>
+            <InputLeftElement>
+                <TimeIcon color={'black'} mt={2} ml={4} />
+            </InputLeftElement>
+            <Input type="text" borderRadius={'full'} bg={'white'} color={'black'} placeholder="18/05/2024 - 11.00 PM" size={'lg'} value={value} onChange={() => null}/>
+                                                                                                    {/* there's an error when value is assigned but onChange is not handled. */}
+        </InputGroup>
+    ));
+
+    return (
+        <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            customInput={<ChakraCustomInput />}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            timeCaption="Time"
+            dateFormat="dd/MM/yyyy - h.mm aa"
+        />
+    );
+}
